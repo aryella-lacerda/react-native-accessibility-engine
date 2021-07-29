111f41d0f5ae44ceac796ff10d64ce2622b4a0c2
Aryella Lacerda <aryella.lacerda@superlogica.com>
2021-07-29 15:48:45 -0300

    chore: reorder imports to create test commit

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,9 +1,9 @@
 import adjustableRoleRequired from './adjustable-role-required';
 import adjustableValueRequired from './adjustable-value-required';
 import buttonRoleRequired from './button-role-required';
+import buttonLabelRequired from './button-label-required';
 import buttonAccessibleRequired from './button-accessible-required';
 import disabledStateRequired from './disabled-state-required';
-import buttonLabelRequired from './button-label-required';
 import linkRoleRequired from './link-role-required';
 import linkRoleMisused from './link-role-misused';
 
