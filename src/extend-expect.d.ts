84cb859ed15a179955d6930da23251d02cacfea6
Aryella Lacerda <aryella.lacerda@gmail.com>
2021-12-04 12:24:09 -0300

    chore: expose jest expect extension type

---
+++ src/extend-expect.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      /**
+       * Checks whether a component conforms to Accessibility A11y best practices.
+       */
+      toBeAccessible(): R;
+    }
+  }
+}
