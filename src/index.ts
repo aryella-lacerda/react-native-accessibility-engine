ac6d851bba4d31b012d030486e89324bf32da2f7
Aryella Lacerda <aryella.lacerda@gmail.com>
2021-12-04 13:33:02 -0300

    chore: export all types from index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import check from './engine';
 
+// The check function remains for backwards-compatibility.
+// The prefered method is now jest matchers.
 export default {
   check,
 };
+
+export * from './types';
