e63d58d13d389b2a42bf0453a8204ae3853d4283
Emily Seibert <emily.seibert@coinbase.com>
2022-11-10 11:22:01 -0700

    test(toBeAccessible): update tests to include new feature

--- src/matchers/toBeAccessible.test.tsx
+++ src/matchers/toBeAccessible.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { TouchableOpacity, Image } from 'react-native';
+import { AccessibilityError } from '../engine';
 import Icons from 'tests/assets';
+import pressableAccessibleRequired from '../rules/pressable-accessible-required';
 
 const NonAccessibleButton = () => (
   <TouchableOpacity accessible={false}>
@@ -32,4 +34,37 @@ it('should report error if component is not accessible', () => {
 
 it('should not report error if component is accessible', () => {
   expect(() => expect(<Button />).not.toBeAccessible()).toThrow();
+});
+
+it('should allow a returnViolations to be customizeable', () => {
+  // returnViolations defaults to true
+  expect(() => expect(<NonAccessibleButton />).toBeAccessible()).not.toThrow(
+    AccessibilityError
+  );
+  expect(() =>
+    expect(<NonAccessibleButton />).toBeAccessible({ returnViolations: false })
+  ).toThrow(AccessibilityError);
+});
+
+it('should allow custom behavior after jest matcher check ', () => {
+  const testOverrideReturnFunctionality = () => [];
+  expect(() =>
+    expect(<NonAccessibleButton />).toBeAccessible({
+      overrideReturnFunctionality: testOverrideReturnFunctionality,
+    })
+  ).not.toThrow();
+});
+
+it('should allow configurable rules for the jest matcher', () => {
+  expect(() =>
+    expect(<NonAccessibleButton />).toBeAccessible({
+      rules: [],
+    })
+  ).not.toThrow();
+
+  expect(() =>
+    expect(<NonAccessibleButton />).toBeAccessible({
+      rules: [pressableAccessibleRequired],
+    })
+  ).toThrow(pressableAccessibleRequired.help.problem);
 });
